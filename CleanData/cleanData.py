03f2c3f604a37df1c371c6ac0eba41022cb8a017
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-20 15:43:38 -0700

    clean data

--- CleanData/cleanData.py
+++ CleanData/cleanData.py
@@ -1,10 +1,22 @@
 import json
+import re
+from ast import literal_eval
 
-# data = open("original/ads.txt", "r")
-# for line in data:
-#     print(line)
-with open("original/ads.txt") as jsonFile:
-    data = json.load(jsonFile)
-    # print(data["campaignId"])
+data = open("original/ads.txt", "r")
+i = 0
+for line in data:
+    # print(line)
+    jsondata = json.loads(line)
+    print(jsondata)
+    newAdId = jsondata["adId"] + 2000 + i
+    print(newAdId)
+    newTitle = jsondata["title"].replace(",", "")
+    newTitle = newTitle.replace("-", "")
+    newTitle = newTitle.replace(".", "")
+    newTitle = re.sub("\s+", " ", newTitle)
+    print(newTitle)
+
+    i = i + 1
+    break;
 
 
