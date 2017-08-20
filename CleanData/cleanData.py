d62585f3038064e17b085392591a324305772a6a
zzhang115 <zzhang115@dons.usfca.edu>
2017-08-20 16:14:19 -0700

    clean data done

--- CleanData/cleanData.py
+++ CleanData/cleanData.py
@@ -1,22 +1,51 @@
 import json
 import re
+import random
 from ast import literal_eval
 
-data = open("original/ads.txt", "r")
-i = 0
-for line in data:
-    # print(line)
-    jsondata = json.loads(line)
-    print(jsondata)
-    newAdId = jsondata["adId"] + 2000 + i
-    print(newAdId)
-    newTitle = jsondata["title"].replace(",", "")
-    newTitle = newTitle.replace("-", "")
-    newTitle = newTitle.replace(".", "")
-    newTitle = re.sub("\s+", " ", newTitle)
-    print(newTitle)
+with open("original/ads.txt", "r") as data:
+    i = 0
+    newData = ""
+    for line in data:
+        # print(line)
+        newAd = {}
+        jsondata = json.loads(line)
+        newAdId = jsondata["adId"] + 2000 + i
+        newTitle = jsondata["title"].replace(",", "")
+        newTitle = newTitle.replace("-", "")
+        newTitle = newTitle.replace(".", "")
+        newTitle = re.sub("\s+", " ", newTitle).lower()
+        newKeyWords = jsondata["keyWords"]
+        for title in newTitle.split(" "):
+            newKeyWords.append(title)
+        newPrice = random.randint(1, 100000)
+        newQuery = jsondata["query"].lower()
 
-    i = i + 1
-    break;
+        newAd["category"] = jsondata["category"]
+        newAd["query"] = newQuery
+        newAd["campaignId"] = jsondata["campaignId"]
+        newAd["title"] = newTitle
+        newAd["price"] = newPrice
+        newAd["relevanceScore"] = jsondata["relevanceScore"]
+        newAd["brand"] = jsondata["brand"]
+        newAd["pClick"] = jsondata["pClick"]
+        newAd["thumbnail"] = jsondata["thumbnail"]
+        newAd["costPerClick"] = jsondata["costPerClick"]
+        newAd["bidPrice"] = jsondata["bidPrice"]
+        newAd["query_group_id"] = jsondata["query_group_id"]
+        newAd["position"] = jsondata["position"]
+        newAd["keyWords"] = newKeyWords
+        newAd["adId"] = newAdId
+        newAd["detail_url"] = jsondata["detail_url"]
+        newAd["rankScore"] = jsondata["rankScore"]
+        newAd["qualityScore"] = jsondata["qualityScore"]
+
+        newData = newData + str(newAd) + "\n"
+        i = i + 1
+print(newData)
+
+with open("cleaned/cleanedAds.txt", "w") as newDataFile:
+    newDataFile.write(newData)
+    newDataFile.close()
 
 
